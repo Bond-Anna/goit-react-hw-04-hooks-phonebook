c26f1f0b7b6dff69d72b1ab8711479c0e0a6c955
Anna Bondarenko <anna93bondarenko@gmail.com>
2021-09-19 20:48:41 +0300

    добавляет хуки

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import { useEffect, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { Form } from './components/form/form';
+import { Filter } from './components/filter/filter';
+import { ContactList } from './components/contacts/contactList';
+
+export const App = () => {
+  const [contacts, setContacts] = useState(() => {
+    return (
+      JSON.parse(localStorage.getItem('contacts')) ?? [
+        { name: 'Rosie Simpson', id: uuidv4(), number: '459-12-56' },
+        { name: 'Hermione Kline', id: uuidv4(), number: '443-89-12' },
+        { name: 'Eden Clements', id: uuidv4(), number: '645-17-79' },
+      ]
+    );
+  });
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const addContact = ({ name, number }) => {
+    const contact = {
+      name,
+      number,
+      id: uuidv4(),
+    };
+    if (
+      contacts.find(
+        cont => cont.name.toLowerCase() === contact.name.toLowerCase()
+      )
+    ) {
+      window.alert(`${contact.name} is already in contacts`);
+      return;
+    } else setContacts([contact, ...contacts]);
+  };
+
+  const onFilterChange = e => {
+    setFilter(e.currentTarget.value);
+  };
+
+  const contactsFilter = () => {
+    const lowFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(lowFilter)
+    );
+  };
+  const filtredContacts = contactsFilter();
+
+  const deleteContact = contactId => {
+    setContacts(contacts.filter(contact => contact.id !== contactId));
+  };
+
+  return (
+    <div className="App">
+      <h1 className="title">Phonebook</h1>
+      <Form onSubmit={addContact} />
+      <h2 className="title">Contacts</h2>
+      <Filter value={filter} onChange={onFilterChange} />
+      <ContactList contacts={filtredContacts} onDeleteBtn={deleteContact} />
+    </div>
+  );
+};
---
+++ src/components/form/form.jsx
@@ -0,0 +1,68 @@
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+import css from './form.module.css';
+
+export const Form = ({ onSubmit }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+  const state = { name, number };
+
+  const onChange = e => {
+    switch (e.currentTarget.name) {
+      case 'name':
+        setName(e.currentTarget.value);
+        break;
+      case 'number':
+        setNumber(e.currentTarget.value);
+        break;
+      default:
+        return;
+    }
+  };
+
+  const onFormSubmit = e => {
+    e.preventDefault();
+    onSubmit(state);
+
+    reset();
+  };
+
+  const reset = () => {
+    setName('');
+    setNumber('');
+  };
+
+  return (
+    <form onSubmit={onFormSubmit} className={css.form}>
+      <label className={css.labelField}>
+        <span className={css.label}>Name</span>
+        <input
+          type="text"
+          name="name"
+          value={name}
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+          required
+          onChange={onChange}
+          className={css.input}
+        />
+      </label>
+      <label className={css.labelField}>
+        <span className={css.label}>Number</span>
+        <input
+          type="tel"
+          name="number"
+          value={number}
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+          required
+          onChange={onChange}
+          className={css.input}
+        />
+      </label>
+      <button type="submit" className={css.formBtn}>
+        Add contact
+      </button>
+    </form>
+  );
+};
